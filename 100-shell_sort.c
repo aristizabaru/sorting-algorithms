077f52f2d6b21ba75a41b1646d4f8a85f937523d
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-01 02:31:39 +0000

    First commit

---
+++ 100-shell_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using the
+ * Quick sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
+ *
+ * Return: void
+ */
+void shell_sort(int *array, size_t size)
+{
+        int interval = 1, temp, i, j;
+
+        for (interval = interval * 3 + 1; interval > 0; interval = (interval - 1) / 3)
+        {
+                for (i = interval; i < (int)size; i += 1)
+                {
+                        temp = array[i];
+                        for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
+                        {
+                                array[j] = array[j - interval];
+                        }
+                        array[j] = temp;
+                }
+                print_array(array, size);
+        }
+}
