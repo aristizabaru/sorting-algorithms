26569efb4268dba0294a01d389f35f669eba6fee
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-02 14:06:09 +0000

    Feat(add protoypes): merge_sort(), merge()

--- sort.h
+++ sort.h
@@ -34,5 +34,7 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap(listint_t **list, listint_t *node);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge(int *array, int size, int mid);
 
 #endif
