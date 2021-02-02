c2ad1cbbd52565d18113bd955bd9bc3e3d7e9f50
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-02 02:19:43 +0000

    Feat: add cocktail_sort_list() and swap() prototypes

--- sort.h
+++ sort.h
@@ -31,5 +31,7 @@ void quick_sort(int *array, size_t size);
 void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size);
 size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap(listint_t **list, listint_t *node);
 
 #endif
