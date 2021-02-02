b67edbb4cf2b5e0aaf1e91c6339ad0d1d45f4926
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-02 11:21:28 +0000

    Feat: add counting_sort() prototype

--- sort.h
+++ sort.h
@@ -33,5 +33,6 @@ size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap(listint_t **list, listint_t *node);
+void counting_sort(int *array, size_t size);
 
 #endif
