4f06f9f751bd93ba9fd23110f04f00433591d68f
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-02 14:19:04 +0000

    Doc(merge()): add documentation for the function

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -7,36 +7,42 @@
  */
 void merge_sort(int *array, size_t size)
 {
-        int mid = size / 2;
+	int mid = size / 2;
 
-        if (array == NULL || size < 2)
-                return;
-        merge_sort(array, mid);
-        merge_sort(array + mid, size - mid);
-        merge(array, size, mid);
+	if (array == NULL || size < 2)
+		return;
+	merge_sort(array, mid);
+	merge_sort(array + mid, size - mid);
+	merge(array, size, mid);
 }
+/**
+ * merge - merge two arrays to sort them
+ * @array: Pointer to array of int
+ * @mid: lower bound
+ * @size: upper bound
+ */
 void merge(int *array, int size, int mid)
 {
-        int i, j, k;
-        int *temp = malloc(size * sizeof(int));
+	int i, j, k;
+	int *temp = malloc(size * sizeof(int));
 
-        printf("Merging...\n");
-        printf("[left]: ");
-        print_array(array, mid);
-        for (i = 0, j = mid, k = 0; k < size; k++)
-        {
-                temp[k] = j == size             ? array[i++]
-                          : i == mid            ? array[j++]
-                          : array[j] < array[i] ? array[j++]
-                                                : array[i++];
-        }
-        printf("[right]: ");
-        print_array(array + mid, size - i);
-        for (i = 0; i < size; i++)
-        {
-                array[i] = temp[i];
-        }
-        printf("[Done]: ");
-        print_array(array, size);
-        free(temp);
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array, mid);
+	for (i = 0, j = mid, k = 0; k < size; k++)
+	{
+		temp[k] = j == size		? array[i++]
+			  : i == mid		? array[j++]
+			  : array[j] < array[i] ? array[j++]
+						: array[i++];
+	}
+	printf("[right]: ");
+	print_array(array + mid, size - i);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = temp[i];
+	}
+	printf("[Done]: ");
+	print_array(array, size);
+	free(temp);
 }
